cff78f429334bbff21f5c28e54cf48b02f542af7
Skipcast <jonathanjl99@gmail.com>
2021-08-26 14:10:37 +0200

    Moved CoreInitPatch to run first in JumpKing.Program.Run

--- JKMP.Patcher/Patches/JumpKing/CoreInitPatch.cs
+++ JKMP.Patcher/Patches/JumpKing/CoreInitPatch.cs
@@ -10,22 +10,22 @@ namespace JKMP.Patcher.Patches.JumpKing
 
         public bool CheckIsPatched(ModuleDefinition module, ModuleDefinition coreModule)
         {
-            var gameType = module.GetType("JumpKing.Game1") ?? throw new PatchException("JumpKing.Game1 type could not be found");
-            var initializeMethod = gameType.FindMethod("Initialize", "Game1 type could not be found");
+            var gameType = module.GetType("JumpKing.Program") ?? throw new PatchException("JumpKing.Program type could not be found");
+            var initializeMethod = gameType.FindMethod("Run", "JumpKing.Program.Run method could not be found");
 
             return initializeMethod.ContainsMethodCall("JKMP.Core.JKMP", "Initialize");
         }
 
         public void Patch(ModuleDefinition module, ModuleDefinition coreModule)
         {
-            var gameType = module.GetType("JumpKing.Game1") ?? throw new PatchException("JumpKing.Game1 type could not be found");
-            var initializeMethod = gameType.FindMethod("Initialize", "Initialize method could not be found");
+            var gameType = module.GetType("JumpKing.Program") ?? throw new PatchException("JumpKing.Program type could not be found");
+            var initializeMethod = gameType.FindMethod("Run", "JumpKing.Program.Run method could not be found");
             var coreInitializeMethod = coreModule.GetType("JKMP.Core.JKMP").FindMethod("Initialize", "JKMP.Initialize could not be found");
 
             var il = initializeMethod.Body.GetILProcessor();
             var call = il.Create(OpCodes.Call, initializeMethod.Module.ImportReference(coreInitializeMethod));
 
-            il.InsertBefore(initializeMethod.Body.Instructions.Last(), call);
+            il.InsertBefore(initializeMethod.Body.Instructions.First(), call);
         }
     }
 }
